bc05aba254bed1ce12d98e7e9696e7e0e9b2e82b
Mélina Deneuve <na-li-me@hotmail.fr>
2022-03-29 15:47:21 -0400

    1st commit

--- src/test/java/soen/tutorial/ArithmeticOperationTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationTest.java
@@ -15,7 +15,7 @@ public class ArithmeticOperationTest {
 		Integer actual = operations.addOrSub(2, 6);
 		Integer expected = 8;
 		//assertEquals(expected, actual);
-		Assert.fail("test must fail");
+		fail("test must fail");
 	}
     /*@Test
     public void testSub()
